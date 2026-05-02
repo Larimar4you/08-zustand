13cf1248c6f7a61118c92ed4ffb05c07b2f327db
Larimar4you <Zajanezlaja@gmail.com>
2026-05-02 02:01:39 +0200

    GO

--- app/NotFoundClient.tsx
+++ app/NotFoundClient.tsx
@@ -10,7 +10,7 @@ export default function NotFoundClient() {
   useEffect(() => {
     const timer = setTimeout(() => {
       router.push("/");
-    }, 5000);
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, [router]);
